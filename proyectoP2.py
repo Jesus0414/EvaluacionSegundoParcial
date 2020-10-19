403b4240e3a7fa82b15a8435fe56b8e900f15275
Jesus_0414 <jesus140400@gmail.com>
2020-10-18 23:36:21 -0700

    Final

--- proyectoP2.py
+++ proyectoP2.py
@@ -30,19 +30,105 @@ def abrirArchivo():
 def analizarArchivo():
     global direccionArchivo
     texto = ""
+    letra = ""
+    espacio = ""
+    divisor1 = 100
+    divisor2 = 10000
     waveTexto = read_wave(direccionArchivo)
 
-    #segmento_waveTexto= waveTexto.segment(0, 0.5)
-    #NumLetras = segmento_waveTexto / 100
+    #numero de Letras que tendrá la palabra
+    primerSegmento = []
+    primerSegmento.append(waveTexto.segment(start=0, duration=0.5))
+    frecuenciaPrimerSegmento = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+    tolerancia = 10
 
-    #print(NumLetras)
-    #print("Inicio: " + str(waveTexto.start))
-    #print("Duración: " + str(waveTexto.duration))
-    #print("Frecuencia de muestreo: " + str(waveTexto.framerate))
+    for segmento in primerSegmento:
+        espectroSegmento = segmento.make_spectrum()
+        frecuenciasDominantes = []
+        i = 0
+        for amplitudEspectral in espectroSegmento.hs:
+            if numpy.abs(amplitudEspectral) > 199:
+                frecuenciasDominantes.append(espectroSegmento.fs[i])
+            i = i + 1
+        frecuenciaNumeroLetras = 0
+        for frecuencia in frecuenciasDominantes:
+            for frecuenciaDTMF in frecuenciaPrimerSegmento:
+                if frecuencia > frecuenciaDTMF - tolerancia and frecuencia < frecuenciaDTMF + tolerancia:
+                    frecuenciaNumeroLetras  = frecuenciaDTMF 
+        if frecuenciaNumeroLetras  == 100:
+            letra =  100 / divisor1
+        elif frecuenciaNumeroLetras  == 200:
+            letra =  200 / divisor1
+        elif frecuenciaNumeroLetras  == 300:
+            letra =  300 / divisor1
+        elif frecuenciaNumeroLetras  == 400:
+            letra =  400 / divisor1
+        elif frecuenciaNumeroLetras  == 500:
+            letra =  500 / divisor1
+        elif frecuenciaNumeroLetras  == 600:
+            letra =  600 / divisor1
+        elif frecuenciaNumeroLetras  == 700:
+            letra =  700 / divisor1
+        elif frecuenciaNumeroLetras  == 800:
+            letra =  800 / divisor1
+        elif frecuenciaNumeroLetras  == 900:
+            letra =  900 / divisor1
+        elif frecuenciaNumeroLetras  == 1000:
+            letra =  1000 / divisor1
+        else:
+            letra = "-"
+    print(letra)
+    round(letra)
+    #tamaño del segmento de cada letra
+    segundoSegmento = []
+    segundoSegmento.append(waveTexto.segment(start=0.5, duration=0.5))
+    frecuenciaSegundoSegmento = [1000, 2000, 2500, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]
+    tolerancia2 = 500
 
-    segmentosNumero = []
-    for i in range(6):
-        segmentosNumero.append(waveTexto.segment(start=i*0.5, duration=0.5))
+    for segmento in segundoSegmento:
+        espectroSegmento = segmento.make_spectrum()
+        frecuenciasDominantes = []
+        i = 0
+        for amplitudEspectral in espectroSegmento.hs:
+            if numpy.abs(amplitudEspectral) > 499:
+                frecuenciasDominantes.append(espectroSegmento.fs[i])
+            i = i + 1
+        frecuenciaTamaño = 0
+        for frecuencia in frecuenciasDominantes:
+            for frecuenciaDTMF in frecuenciaSegundoSegmento:
+                if frecuencia > frecuenciaDTMF - tolerancia2 and frecuencia < frecuenciaDTMF + tolerancia2:
+                    frecuenciaTamaño  = frecuenciaDTMF 
+        if frecuenciaTamaño  == 1000:
+            espacio =  1000 / divisor2
+        elif frecuenciaTamaño  == 2000:
+            espacio =  2000 / divisor2
+        elif frecuenciaTamaño  == 2500:
+            espacio =  2500 / divisor2
+        elif frecuenciaTamaño  == 3000:
+            espacio =  3000 / divisor2
+        elif frecuenciaTamaño  == 4000:
+            espacio =  4000 / divisor2
+        elif frecuenciaTamaño  == 5000:
+            espacio =  5000 / divisor2
+        elif frecuenciaTamaño  == 6000:
+            espacio =  6000 / divisor2
+        elif frecuenciaTamaño  == 7000:
+            espacio =  7000 / divisor2
+        elif frecuenciaTamaño  == 8000:
+            espacio =  8000 / divisor2
+        elif frecuenciaTamaño  == 9000:
+            espacio =  9000 / divisor2
+        elif frecuenciaTamaño  == 10000:
+            espacio =  10000 / divisor2
+        else:
+            espacio = "-"
+    print(espacio)
+
+    #decodificador de la palabra
+    segmentoTexto = []
+    float(i)
+    for i in range(int(letra)):
+        segmentoTexto.append(waveTexto.segment(start=1.0+i*float(espacio), duration=float(espacio)))
 
     frecuenciasLetrasDTMF = [200, 240, 280, 320, 360, 400, 440, 480, 520, 
     560, 600, 640, 680, 720, 760, 800, 840, 880, 
@@ -50,12 +136,12 @@ def analizarArchivo():
 
     tolerancia = 10
 
-    for segmento in segmentosNumero:
+    for segmento in segmentoTexto:
         espectroSegmento = segmento.make_spectrum()
         frecuenciasDominantes = []
         i = 0
         for amplitudEspectral in espectroSegmento.hs:
-            if numpy.abs(amplitudEspectral) > 199:
+            if numpy.abs(amplitudEspectral) > 189:
                 frecuenciasDominantes.append(espectroSegmento.fs[i])
             i = i + 1
         frecuenciaLetra = 0
